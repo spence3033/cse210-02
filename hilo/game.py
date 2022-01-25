ce18723485cd5b3f1603e348dab77fbf373edb68
starb520 <97459015+starb520@users.noreply.github.com>
2022-01-25 15:44:10 -0700

    fixed the get_input function in the Game class.

--- hilo/game.py
+++ hilo/game.py
@@ -31,13 +31,14 @@ class Game:
 
     def get_input(self):
         '''Ask the user if the number is higher (h) or lower(l).'''
+        self.score = 0
         print(f"The card is: {self.card_value_1}")
         choose_high_low = input("Higher or Lower? [h/l] ")
         if ((self.card_value_2 > self.card_value_1 and choose_high_low == "h") or
            (self.card_value_2 < self.card_value_1 and choose_high_low == "l")):
             self.score = 100
         else:
-            self.score -= 75
+            self.score = -75
             
 
     def do_updates(self):
@@ -49,8 +50,8 @@ class Game:
            total score. Ask if user wants to continue to play.'''
         print(f"The card was: {self.card_value_2}")
         print(f"Your score is: {self.total_score}")
-        if self.total_score == 0:
-            return
+        if self.total_score <= 0:
+            self.is_playing = False
         else:
             continue_play = input("Play again? [y/n] ")
             self.is_playing = (continue_play == "y")
