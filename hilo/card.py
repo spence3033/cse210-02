5f2e608de7eca9bbcded79e319fddbcbac9a04e8
starb520 <97459015+starb520@users.noreply.github.com>
2022-01-25 15:48:11 -0700

    added a comment to some code we do not need for our project

--- hilo/card.py
+++ hilo/card.py
@@ -14,6 +14,10 @@ class Card:
         self.value = random.randint(1, 13)
 
 
+# This was some code I used to test classes and how to access the attributes
+# and how to use the methods of the class. It is completely irrelevant and 
+# needs to be deleted before turning in our project, but if it is okay to 
+# leave it here for now, it will help me rememember.
 # card = Card()
 # card.random_card()
 # print(card.value)
